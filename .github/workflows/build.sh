b1758e410734d73365dc1bc4391d92b40a513727
sree7k7 <sree7k7@gmail.com>
2023-08-11 14:05:51 +0200

    dss

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "bulding build.shx"
 
+git clone git@github.com:sree7k7/github-action-for-docker.git
+
 git config user.email sree7k7@gmail.com
 git config user.name sree7k7
 
@@ -16,3 +18,9 @@ sed -i 's+sree7k7/my-web:v6+sree7k7/my-web:v7+g' ./deployment.yaml
 echo "2.1"
 
 cat deployment.yaml
+
+git add .
+
+git commit -m "added"
+
+git push 
