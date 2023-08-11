a8bb70e931afc34237854a27d38ec1c199a2f3da
sree7k7 <sree7k7@gmail.com>
2023-08-11 11:12:12 +0200

    test

---
+++ .github/workflows/build.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sh "git config user.email sree7k7@gmail.com"
+sh "git config user.name sree7k7"
+
+sh "cat deployment.yaml"
+# sh "sed -i  's+sree7k7/my-web.*+sree7k7/my-web:${{ steps.meta.outputs.tags }}+g' deployment.yaml"tes
